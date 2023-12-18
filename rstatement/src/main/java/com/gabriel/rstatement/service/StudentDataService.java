38a558b5115b77d7aaa1c3f827df141486eb625b
Paul-Ben Ajene <paulben.ajene@gmail.com>
2023-12-18 18:07:13 +0100

    Upload module added

--- rstatement/src/main/java/com/gabriel/rstatement/service/StudentDataService.java
+++ rstatement/src/main/java/com/gabriel/rstatement/service/StudentDataService.java
@@ -1,19 +1,28 @@
 package com.gabriel.rstatement.service;
 
+import java.io.IOException;
 import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
 
 import com.gabriel.rstatement.dto.StudentDataDto;
 import com.gabriel.rstatement.exceptions.ResourceNotFoundException;
+import com.gabriel.rstatement.models.StudentData;
 
 public interface StudentDataService {
     StudentDataDto createStudentData(StudentDataDto StudentDataDto);
 
     List<StudentDataDto> getAllStudentData();
+    
+    StudentData findByMatNumber(String matNumber);
 
     // StudentDataDto getStudentDataById(Long id) throws ResourceNotFoundException;
     StudentDataDto getAStudentDataById(Long id) throws ResourceNotFoundException;
+    StudentData getStudentDataByID(Long id);
 
     StudentDataDto updateStudentData(StudentDataDto studentDataDto, Long id);
+    void saveAll(List<StudentData> studentDatas);
+    void saveFromCsv(MultipartFile file) throws IOException;
 
     void deleteStudentDatabyId(Long id);
 }
